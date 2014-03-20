711e5d6ed0741aabcabacf06e55a4f7a668ee204
TheKeveloper <superkevinbi@gmail.com>
2014-03-20 15:03:45 -0500

    Created Sprite2D
    Created a Sprite2D file as a basic layout to the class.

---
+++ Sprite2D.cs
@@ -0,0 +1,20 @@
+//Copyright (c) 2014, Kevin L. Bi, All Rights Reserved. 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Media;
+
+namespace XNAHelper
+{ 
+  public class Sprite2D
+  {
+  
+  }
+}
