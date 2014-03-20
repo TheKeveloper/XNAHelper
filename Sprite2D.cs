ab0970f7e37af533d58da96b3a2bd7f2e15c9f0f
Superkb10 <superkevinbi@gmail.com>
2014-03-20 16:00:54 -0500

    Set up Sprite2D class
    Created the basic properties necessary in a Sprite2D class, also added
    the Constructor.

--- Sprite2D.cs
+++ Sprite2D.cs
@@ -13,8 +13,36 @@ using Microsoft.Xna.Framework.Media;
 
 namespace XNAHelper
 { 
-  public class Sprite2D
-  {
-  
-  }
+	public class Sprite2D
+	{
+		public Texture2D Texture;
+		public Color color;
+		public float Width;
+		public float Height;
+		public Vector2 Position;
+		public Vector2 Direction;
+		public float Speed;
+		public Rectangle Rect
+		{
+			get
+			{
+
+			}
+			set
+			{
+
+			}
+		}
+
+		public Sprite2D(Texture2D texture, Vector2 position, Color color)
+		{
+			this.Texture = texture;
+			this.color = color;
+			this.Width = texture.Width;
+			this.Height = texture.Height;
+			this.Position = position;
+			this.Speed = 0;
+			this.Direction = Vector2.Zero;
+		}
+	}
 }
