3b8bee0465e8d10eb1db5d38360c904a670bd08d
Superkb10 <superkevinbi@gmail.com>
2014-05-24 19:53:10 -0500

    Added XNALabel
    Added a Label helper class called XNALabel.

---
+++ XNALabel.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace XNAHelper
+{
+    public class XNALabel
+    {
+        public String Text;
+        public SpriteFont Font;
+        public String Name;
+        public Vector2 Position;
+        public SpriteEffects Effects;
+        public int ZIndex;
+        public Color TextColor;
+        public float Scale;
+
+        public XNALabel()
+        {
+            new XNALabel(String.Empty, null, Vector2.Zero);
+        }
+
+        public XNALabel(String text, SpriteFont font, Vector2 pos)
+        {
+            new XNALabel(text, font, pos, Color.White);
+        }
+
+        public XNALabel(String text, SpriteFont font, Vector2 pos, Color textColor)
+        {
+            new XNALabel(text, font, pos, textColor, SpriteEffects.None, 0, 1, String.Empty);
+        }
+
+        public XNALabel(String text, SpriteFont font, Vector2 pos, Color textColor, SpriteEffects effects, int zIndex, float scale, String name)
+        {
+            this.Text = text;
+            this.Font = font;
+            this.Position = pos;
+            this.TextColor = textColor;
+            this.Effects = effects;
+            this.ZIndex = zIndex;
+            this.Scale = scale;
+            this.Name = name;
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(this.Font, this.Text, this.Position, this.TextColor, 0, this.Position, this.Scale, this.Effects, this.ZIndex);
+        }
+    }
+}
